9bbe2e3e0d4e9d9d094d2f44d36c777371db5bfa
Joseph Finfrock <70216057+4804032140@users.noreply.github.com>
2024-03-03 11:10:16 -0500

    Ported teams.js to firebase

--- functions/routes/teams.js
+++ functions/routes/teams.js
@@ -1,109 +1,117 @@
 const express = require('express')
 const router = express.Router()
-const Team = require('../models/teams.js')
-const User = require('../models/users.js')
-const Project = require('../models/projects.js')
-const Stationary_Map = require('../models/stationary_maps.js')
-const Moving_Map = require('../models/moving_maps.js')
-const Sound_Map = require('../models/sound_maps.js')
-const Nature_Map = require('../models/nature_maps.js')
-const Light_Map = require('../models/light_maps.js')
-const Order_Map = require('../models/order_maps.js')
-const Boundaries_Map = require('../models/boundaries_maps.js')
-const Section_Map = require('../models/section_maps.js')
-const Access_Map = require('../models/access_maps.js')
-const Program_Map = require('../models/program_maps.js')
+const basicDBfoos = require('../databaseFunctions/BasicFunctions.js');
+const arrayDBfoos = require('../databaseFunctions/ArrayFunctions.js');
+const userDBfoos = require('../databaseFunctions/UserFunctions.js');
+const refDBfoos = require('../databaseFunctions/ReferenceFunctions.js');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../utils/config');
+const { json } = require('express');
+const {
+        ACCSESS_MAPS,
+        BOUNDARIES_MAPS,
+        LIGHT_MAPS,
+        MOVING_MAPS,
+        NATURE_MAPS,
+        ORDER_MAPS,
+        PROGRAM_MAPS,
+        PROJECTS,
+        SOUND_MAPS,
+        STATIONARY_MAPS,
+        TEAMS,
+        USERS,
+    } = require('../databaseFunctions/CollectionNames.js');
 
-
-const passport = require('passport')
-const jwt = require('jsonwebtoken')
-const config = require('../utils/config')
-const { json } = require('express')
-
-const { UnauthorizedError, NotFoundError, BadRequestError } = require('../utils/errors')
-const teams = require('../models/teams.js')
+const { UnauthorizedError, NotFoundError, BadRequestError } = require('../utils/errors');
+const teams = require('../models/teams.js');
 
 //route creates a new team
 router.post('', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
-    user = await req.user
-    let newTeam = new Team({
+    let user = await req.user;
+    let newTeam = {
+        _id: {$oid: basicDBfoos.createId()},
         title: req.body.title,
         description: req.body.description,
         users: [{user:user._id, role:'owner'}],
         public: req.body.public
-    })
-
-    const team = await Team.addTeam(newTeam)
-
-    // Add the new team to the user's teams
-    user.teams.push(team._id)
-    user.save()
-
-    res.status(201).json(team)
+    };
+    await basicDBfoos.addObj(newTeam, TEAMS);
+    await arrayDBfoos.addArrayElement(user._id, "teams", "users", newTeam._id);
+    res.status(201).json(newTeam);
 })
 
 //route displays team information
 router.get('/:id', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
-    var team = await Team.findById(req.params.id).populate('projects', 'title description')
-    for(var i = 0; i < team.users.length; i++){
-        const user = await User.findById(team.users[i].user)
-        myRole = team.users[i].role
+    const teamId = await req.params.id;
+    let team = await basicDBfoos.getObj(teamId, TEAMS);
+    for(let i = 0; i < team.projects.length; i++) {
+        const project = await basicDBfoos.getObj(team.projects[i], PROJECTS);
+        team.projects[i] = {title: project.title, description: project.description};
+
+    }
+    for(let i = 0; i < team.users.length; i++) {
+        const user = await basicDBfoos.getObj(team.users[i], USERS);
+        myRole = team.users[i].role;
         team.users[i] = {
                             user: user._id,
                             role: myRole,
                             firstname: user.firstname,
-                            lastname: user.lastname
-                        }
+                            lastname: user.lastname,
+                        };
     }
-    res.status(200).json(team)
+    res.status(200).json(team);
 })
 
 //route updates team information
 router.put('/:id', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
-    user = await req.user
-    team = await Team.findById(req.params.id)
+    const user = await req.user;
+    const team = await req.params.id;
 
-    if (await Team.isAdmin(req.params.id,user._id)){
-        
-        let newTeam = new Team({
-            title: (req.body.title ? req.body.title : team.title),
-            description: (req.body.description ? req.body.description : team.description),
-            public: (req.body.public ? req.body.public : team.public)
-        })
-        
-        res.status(201).json(await Team.updateTeam(req.params.id,newTeam))
+    if (await userDBfoos.isAdmin(team, user._id) === false)
+    {
+        res.status(201).json("not admin");
+        return;
     }
 
-    else{
-        throw new UnauthorizedError('You do not have permision to perform this operation')
+    const newTeam = {
+        title: (req.body.title ? req.body.title : team.title),
+        description: (req.body.description ? req.body.description : team.description),
+        public: (typeof req.body.public !== "undefined" ? req.body.public : team.public)
     }
+    await basicDBfoos.updateObj(team, newTeam, TEAMS);
+    res.status(201).json("done");
 })
 
 //deletes an entire team
-router.delete('/:id', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
-    user = await req.user
-    team = await Team.findById(req.params.id)
-    if(await Team.isOwner(team._id,user._id)){
+router.delete('/:id', passport.authenticate('jwt',{session:false}),async (req, res, next) => {
+    const teamId = await req.params.id;
+    user = await req.user;
+    // user = await req.user;
+    team = await basicDBfoos.getObj(teamId, TEAMS);
+    const authorized = await userDBfoos.isAdmin(team._id, user);
+    console.log(authorized);
+    if(authorized) {
     
         //delete ALL of the map collections which contain a projectId which belongs to the team
         if(team.projects.length){
-            for(var i = 0; i < team.projects.length; i++ ){
-                proj = team.projects[i]
-                await Stationary_Map.projectCleanup(proj)
-                await Moving_Map.projectCleanup(proj)
-                await Sound_Map.projectCleanup(proj)
-                await Nature_Map.projectCleanup(proj)
-                await Light_Map.projectCleanup(proj)
-                await Order_Map.projectCleanup(proj)
-                await Boundaries_Map.projectCleanup(proj)
-                await Section_Map.projectCleanup(proj)
-                await Access_Map.projectCleanup(proj)
-                await Program_Map.projectCleanup(proj)
+            for(let i = 0; i < team.projects.length; i++ ) {
+                proj = team.projects[i];
+                await refDBfoos.projectCleanup(proj, ACCSESS_MAPS);
+                await refDBfoos.projectCleanup(proj, BOUNDARIES_MAPS);
+                await refDBfoos.projectCleanup(proj, LIGHT_MAPS);
+                await refDBfoos.projectCleanup(proj, MOVING_MAPS);
+                await refDBfoos.projectCleanup(proj, NATURE_MAPS);
+                await refDBfoos.projectCleanup(proj, ORDER_MAPS);
+                await refDBfoos.projectCleanup(proj, PROGRAM_MAPS);
+                await refDBfoos.projectCleanup(proj, SECTION_MAPS);
+                await refDBfoos.projectCleanup(proj, SOUND_MAPS);
+                await refDBfoos.projectCleanup(proj, STATIONARY_MAPS);
             }
     
-            await Project.teamCleanup(team._id)
+            await basicDBfoos.teamCleanup(teamId);
         }
-        res.json(await Team.deleteTeam(team._id))
+        res.json(await basicDBfoos.deleteObj(teamId, TEAMS));
 
     }
     else{
@@ -112,33 +120,35 @@ router.delete('/:id', passport.authenticate('jwt',{session:false}), async (req, 
 
 })
 
-//route sends invitation to other users signed up to the application
 router.post('/:id/invites', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
-    user = await req.user
-    team = await Team.findById(req.params.id)
-
-    if(await Team.isAdmin(team._id,user._id)){
+    const teamId = req.params.id;
+    const user = await req.user;
+    const team = await basicDBfoos.getObj(teamId, TEAMS);
+    if (!team)
+    {
+        res.status(400).json(team);
+    }
+    const authorized = await userDBfoos.isAdmin(team._id,user._id);
+    if(authorized) {
         
-        newMember = await User.findUserByEmail(req.body.userEmail)
-
+        const newMember = await userDBfoos.findUserByEmail(req.body.userEmail);
         if(newMember == null)
-            throw new NotFoundError('No user with provided email exists')
+            throw new NotFoundError('No user with provided email exists');
 
-        if(newMember.teams.includes(team._id))
-            throw new BadRequestError('User is already a member of team')
+        if(newMember.teams.some(element => element.$oid === team._id.$oid))
+            throw new BadRequestError('User is already a member of team');
 
-        if(newMember.invites.includes(team._id))
-            throw new BadRequestError('User already has invite to team')
+        if(newMember.invites.some(element => element.$oid === team._id.$oid))
+            throw new BadRequestError('User already has invite to team');
         
         else{
-            await User.addInvite(newMember._id, team._id)
-            res.status(200).json("Sent invite to new User")
+            await arrayDBfoos.addArrayElement(newMember._id, "invites", USERS, team._id);
+            res.status(200).json("Sent invite to new User");
         }
     }
     else{
-        throw new UnauthorizedError('You do not have permision to perform this operation')
+        throw new UnauthorizedError('You do not have permision to perform this operation');
     }
-
 })
 
 module.exports = router
