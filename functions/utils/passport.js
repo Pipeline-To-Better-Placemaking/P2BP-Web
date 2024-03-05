22a5279c99d73f9ed389cba213e603dfb58faef7
Joseph Finfrock <70216057+4804032140@users.noreply.github.com>
2024-03-05 14:43:56 -0500

    Update passport.js

--- functions/utils/passport.js
+++ functions/utils/passport.js
@@ -1,25 +1,20 @@
-//decodes jwt key which authorizes access to most routes
-
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
-const User = require('../models/users.js')
+const basicDBfoos = require('../databaseFunctions/BasicFunctions.js');
 const config = require('./config.js')
+const {USERS} = require('../databaseFunctions/CollectionNames.js');
 
-module.exports = function(passport){
+module.exports = function async(passport){
     var opts = {}
     opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
     opts.secretOrKey = config.PRIVATE_KEY
-    passport.use(new JwtStrategy(opts,
-        (jwt_payload, done) => {
-            User.findById(jwt_payload._id, (err, user) => {
-                if (err) return done(err)
-                if (user) {
-                    return done(null, user)
-                }
-                else{
-                    return done(null, false)
-                }
-            })
+
+    passport.use(new JwtStrategy(opts, async(jwt_payload, done) => {
+        const userId = await basicDBfoos.getObj(jwt_payload._id, USERS);
+        if(userId)
+        {
+            return done(null,userId)
         }
-    ))
+        return done(err, false)
+    }))
 }
